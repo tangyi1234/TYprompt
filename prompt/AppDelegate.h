2d9107e6de24393dcbbc6fbadc7cf5fd9a2a9042
tangyi1234 <xiyin@tangyidemacbook-pro.local>
2015-09-29 10:37:26 +0800

    Initial Commit

---
+++ prompt/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  prompt
+//
+//  Created by 汤义 on 15/9/29.
+//  Copyright © 2015年 汤义. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ prompt/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  prompt
+//
+//  Created by 汤义 on 15/9/29.
+//  Copyright © 2015年 汤义. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
